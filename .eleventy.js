ec3b1664a6156e43c51092fc34bc63ff39ba69c6
SudoDoNotBeEvil <makecodenotwar@tutanota.com>
2024-04-08 15:08:19 +0400

    slug replace2

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/assets/data");
   eleventyConfig.addPassthroughCopy("./assets");
   eleventyConfig.addPassthroughCopy("./assets/data");
   eleventyConfig.addPassthroughCopy("./src/admin");
