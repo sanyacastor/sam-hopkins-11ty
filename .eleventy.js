e80055f2d3ca11f61c7f19735d0277cd4920947d
SudoDoNotBeEvil <makecodenotwar@tutanota.com>
2024-04-08 14:36:00 +0400

    added /assets/data passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./assets");
+  eleventyConfig.addPassthroughCopy("./assets/data");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
   return {
