7ad53abd62943ef80a4ce75c4b07b9b7a54b2a60
SudoDoNotBeEvil <makecodenotwar@tutanota.com>
2024-04-08 12:50:10 +0400

    moved admin to src

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./assets");
-  eleventyConfig.addPassthroughCopy("./admin");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   return {
     dir: {
