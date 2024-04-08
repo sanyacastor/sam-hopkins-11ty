23b038751e53b9e217a29eda569b3bd9b56c2516
SudoDoNotBeEvil <makecodenotwar@tutanota.com>
2024-04-08 12:44:48 +0400

    admin update

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./assets");
+  eleventyConfig.addPassthroughCopy("./admin");
+
   return {
     dir: {
       input: 'src',
