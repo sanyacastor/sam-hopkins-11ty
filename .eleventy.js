145c0449f11b2c6fbd9381c2aa5a30bdb91f1c8c
Alexander Ilyin <alexander.i@devhouse.pro>
2024-04-12 13:32:05 +0400

    feat: Karelia font

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,17 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/assets");
-  eleventyConfig.addPassthroughCopy("./src/assets/data");
-  eleventyConfig.addPassthroughCopy("./assets");
-  eleventyConfig.addPassthroughCopy("./assets/data");
-  eleventyConfig.addPassthroughCopy("./src/admin");
-  eleventyConfig.addPassthroughCopy("./assets/data/projects");
-  eleventyConfig.addPassthroughCopy("./assets/data/projects/images");
+  eleventyConfig.addPassthroughCopy('./src/assets')
+  eleventyConfig.addPassthroughCopy('./src/assets/data')
+  eleventyConfig.addPassthroughCopy('./assets')
+  eleventyConfig.addPassthroughCopy('./assets/data')
+  eleventyConfig.addPassthroughCopy('./src/admin')
+  eleventyConfig.addPassthroughCopy('./assets/data/projects')
+  eleventyConfig.addPassthroughCopy('./assets/data/projects/images')
 
+  eleventyConfig.addPassthroughCopy('**/*.woff2')
 
-  eleventyConfig.addCollection("posts", function (collectionAPI) {
-    return collectionAPI.getFilteredByGlob("./assets/data/projects/*.md");
-  });
+  eleventyConfig.addCollection('posts', function (collectionAPI) {
+    return collectionAPI.getFilteredByGlob('./assets/data/projects/*.md')
+  })
 
   return {
     dir: {
