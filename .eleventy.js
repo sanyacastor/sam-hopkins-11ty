ef441809ae7c26ffaa7c43ef576701c7c5ad7335
Alexander Ilyin <alexander.i@devhouse.pro>
2024-04-12 13:32:05 +0400

    feat: Karelia font

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./assets/data");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
+  eleventyConfig.addPassthroughCopy('**/*.woff2');
+  
   return {
     dir: {
       input: 'src',
