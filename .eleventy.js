4f35b92766eb0424b82aac99af517530fce1e1dd
Alexander Ilyin <alexander.i@devhouse.pro>
2024-04-13 23:26:44 +0400

    Revert "added path to .md to .eleventy.js"
    This reverts commit 697e3f8f23ed278d8fecc3fb18cacbd162f6d2fc.

--- .eleventy.js
+++ .eleventy.js
@@ -4,18 +4,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./assets");
   eleventyConfig.addPassthroughCopy("./assets/data");
   eleventyConfig.addPassthroughCopy("./src/admin");
-  eleventyConfig.addPassthroughCopy("./assets/data/projects");
-  eleventyConfig.addPassthroughCopy("./assets/data/projects/images");
-
-
-  eleventyConfig.addCollection("posts", function (collectionAPI) {
-    return collectionAPI.getFilteredByGlob("./assets/data/projects/*.md");
-  });
 
   return {
     dir: {
       input: 'src',
-      includes: '_includes',
       output: 'public',
     },
   }
