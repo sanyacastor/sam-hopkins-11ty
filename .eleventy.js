23581ec1dd1e5794a0c9f58c29aed14fd1f1b43a
Alexander Ilyin <alexander.i@devhouse.pro>
2024-05-14 13:00:30 +0400

    feat: image shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,33 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/fonts/KareliaWeb-Bold.woff2')
   eleventyConfig.addPassthroughCopy('./src/fonts/KareliaWeb-Regular.woff2')
 
+  eleventyConfig.addShortcode(
+    'image',
+    async function (src, alt, className, sizes) {
+      let metadata = await Image(`src${src}`, {
+        widths: [300, 600, 1000],
+        formats: ['avif', 'jpeg'],
+        outputDir: './img/',
+        filenameFormat: function (_, src, width, format) {
+          const extension = path.extname(src)
+          const name = path.basename(src, extension)
+
+          return `${name}-${width}w.${format}`
+        },
+      })
+
+      let imageAttributes = {
+        alt: alt || '',
+        sizes: sizes || '(min-width: 30em) 50vw, 100vw',
+        loading: 'lazy',
+        decoding: 'async',
+        class: className,
+      }
+
+      return Image.generateHTML(metadata, imageAttributes)
+    },
+  )
+
   return {
     dir: {
       input: 'src',
