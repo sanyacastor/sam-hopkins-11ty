641acfa887fd60d73d27a0c0294e387744598686
Alexander Ilyin <alexander.i@devhouse.pro>
2024-05-25 17:16:18 +0400

    feat: mobile view images

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,40 @@ const autoprefixer = require('autoprefixer')
 const csso = require('postcss-csso')
 const pimport = require('postcss-import')
 
+const eleventyImage = require('markdown-it-eleventy-img')
+
+const IMAGE_CONFIG = {
+  widths: [300, 600, 1000],
+  formats: ['avif', 'jpeg'],
+  outputDir: './public/img/',
+  filenameFormat: function (_, src, width, format) {
+    const extension = path.extname(src)
+    const name = path.basename(src, extension)
+
+    return `${name}-${width}w.${format}`
+  },
+}
+
+const IMAGE_SIZES = '(min-width: 320px) 50vw, 100vw'
+
+const markdownIt = require('markdown-it')({
+  html: true,
+  breaks: true,
+}).use(eleventyImage, {
+  imgOptions: IMAGE_CONFIG,
+  resolvePath: (filepath, env) => path.join('./src/', filepath),
+  globalAttributes: {
+    class: 'markdown-image--mobile',
+    decoding: 'async',
+    sizes: IMAGE_SIZES,
+  },
+})
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addTemplateFormats('css')
+
+  // Markdown parser
+  eleventyConfig.setLibrary('md', markdownIt)
 
   // Assets
   eleventyConfig.addPassthroughCopy('./src/assets/icons')
@@ -29,40 +61,28 @@ module.exports = function (eleventyConfig) {
     outputFileExtension: 'css',
     compile: async (inputContent, inputPath) => {
       if (inputPath !== './src/styles/index.css') {
-        return;
+        return
       }
-  
+
       return async () => {
-        let output = await postcss([
-          pimport,
-          autoprefixer,
-          csso
-        ]).process(inputContent, { from: inputPath });
-  
-        return output.css;
+        let output = await postcss([pimport, autoprefixer, csso]).process(
+          inputContent,
+          { from: inputPath },
+        )
+
+        return output.css
       }
-    }
-  });
+    },
+  })
 
   eleventyConfig.addShortcode(
     'image',
     async function (src, alt, className, sizes) {
       try {
-        let metadata = await Image(`src${src}`, {
-          widths: [300, 600, 1000],
-          formats: ['avif', 'jpeg'],
-          outputDir: './public/img/',
-          filenameFormat: function (_, src, width, format) {
-            const extension = path.extname(src)
-            const name = path.basename(src, extension)
-
-            return `${name}-${width}w.${format}`
-          },
-        })
-
-        let imageAttributes = {
+        const metadata = await Image(`src${src}`, IMAGE_CONFIG)
+        const imageAttributes = {
           alt: alt || '',
-          sizes: sizes || '(min-width: 320px) 50vw, 100vw',
+          sizes: sizes || IMAGE_SIZES ,
           loading: 'lazy',
           decoding: 'async',
           class: className,
